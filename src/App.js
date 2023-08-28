dde88a4c3e3f4470a6fd35019fcdcb848d2754c0
gunggme <ekfrifajfl2019@gmail.com>
2023-08-28 19:58:41 +0900

    코딩애플 3강 시청 완료

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -5,8 +7,9 @@ import { useState } from 'react';
 function App() {
 
   let post = '오늘의 일기 작성';
-  let [text, setText1] = useState(['남자코트 추천', '강남 우동맛짐', '리액트 독학'])
-
+  let [text, setText] = useState(['남자코트 추천', '강남 우동맛짐', '리액트 독학'])
+  let [like, setLike] = useState(0);
+  
   return (
     <div className="App">
       <div className="black-nav">
@@ -15,7 +18,7 @@ function App() {
         </h4>
       </div>
       <div className='list'>
-        <h4>{ text[0] }</h4>
+        <h4>{ text[0] } <span onClick={ ()=>{setLike(like+1)}  }>👍</span> {like} </h4>
         <p>8월 28일 발행</p>
       </div>
       <div className='list'>
