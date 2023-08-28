0cc745185526d07179f0b556097cc6af6953cce9
gunggme <ekfrifajfl2019@gmail.com>
2023-08-28 17:18:58 +0900

    3강 시청완료 useState사용법 등등

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '오늘의 일기 작성';
+  let [text, setText1] = useState(['남자코트 추천', '강남 우동맛짐', '리액트 독학'])
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>
-          블로그일꺼야
+          React Velog
         </h4>
       </div>
-      <h4>{post}</h4>
+      <div className='list'>
+        <h4>{ text[0] }</h4>
+        <p>8월 28일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ text[1] }</h4>
+        <p>8월 28일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ text[2] }</h4>
+        <p>8월 28일 발행</p>
+      </div>
     </div>
   );
 }
